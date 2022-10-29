4edb275d0637ce26e1202ecef0b02c5286a8e526
Sendy Joan K <69149460+sendyjoan@users.noreply.github.com>
2022-10-29 16:35:02 +0700

    Finish the project

--- app/src/main/java/com/example/lemonade/MainActivity.kt
+++ app/src/main/java/com/example/lemonade/MainActivity.kt
@@ -3,17 +3,26 @@ package com.example.lemonade
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.BlendMode.Companion.Color
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.lemonade.ui.theme.LemonadeTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,17 +38,100 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun LemonApp(){
+    // Variabel ini digunakan untuk menentukan step ke berapa yang saat ini dijalankan
+    var currentStep by remember { mutableStateOf(1)}
+    // Variabel ini digunakan untuk menentukan berapa banyak klik dalam proses pemerasan lemon
+    var squeezeCount by remember { mutableStateOf(0)}
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colors.background
     ){
-
+        when (currentStep) {
+            1 -> {
+                LemonTextAndImage(
+                    textLabelResourceId = R.string.lemon_select,
+                    drawableResourceId = R.drawable.lemon_tree,
+                    contentDescriptionResourceId = R.string.lemon_tree_content_description,
+                    onImageClick = {
+                        // Jika gambar di click maka step akan lanjut kedalam step kedua dengan mengubah nilai variabel currentStep ke dalam step ke 2
+                        currentStep = 2
+                        // Jika gambar di click maka akan mendeklarasikan atau mengubah nilai dari variabel squezz menjadi angka random antara 2 sampai 4
+                        squeezeCount = (2..4).random()
+                    }
+                )
+            }
+            2 -> {
+                LemonTextAndImage(
+                    textLabelResourceId = R.string.lemon_squeeze,
+                    drawableResourceId = R.drawable.lemon_squeeze,
+                    contentDescriptionResourceId = R.string.lemon_content_description,
+                    onImageClick = {
+                        // jika di klik maka nilai dari variabel squezz akan berkurang sebanyak 1
+                        squeezeCount--
+                        // melakukan pengecekan apabila squezze telah habis maka akan pindah ke tampilan ke 3
+                        if (squeezeCount == 0) {
+                            currentStep = 3
+                        }
+                    }
+                )
+            }
+            3 -> {
+                LemonTextAndImage(
+                    textLabelResourceId = R.string.lemon_drink,
+                    drawableResourceId = R.drawable.lemon_drink,
+                    contentDescriptionResourceId = R.string.lemonade_content_description,
+                    onImageClick = {
+                        currentStep = 4
+                    }
+                )
+            }
+            4 -> {
+                LemonTextAndImage(
+                    textLabelResourceId = R.string.lemon_empty_glass,
+                    drawableResourceId = R.drawable.lemon_restart,
+                    contentDescriptionResourceId = R.string.empty_glass_content_description,
+                    onImageClick = {
+                        currentStep = 1
+                    }
+                )
+            }
+        }
     }
 }
 
 @Composable
-fun LemonTextAndImage(){
-    
+fun LemonTextAndImage(
+    textLabelResourceId: Int,
+    drawableResourceId: Int,
+    contentDescriptionResourceId: Int,
+    onImageClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier.fillMaxSize()
+    ) {
+        Text(
+            text = stringResource(textLabelResourceId),
+            fontSize = 18.sp
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Image(
+            painter = painterResource(drawableResourceId),
+            contentDescription = stringResource(contentDescriptionResourceId),
+            modifier = Modifier
+                .wrapContentSize()
+                .clickable(
+                    onClick = onImageClick
+                )
+                .border(
+                    BorderStroke(2.dp, Color(105, 205, 216)),
+                    shape = RoundedCornerShape(4.dp)
+                )
+                .padding(16.dp)
+        )
+    }
 }
 
 @Preview(showBackground = true)
